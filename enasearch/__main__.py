c39e513195262757f5e50a4d059b2571c53b12ae
Bérénice Batut <bebatut@gmail.com>
2017-03-30 16:43:03 +0200

    Fix call to ebisearch

--- enasearch/__main__.py
+++ enasearch/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import click
-import ebisearch
+import enasearch
 
 
 @click.group()
@@ -12,7 +12,7 @@ def main():
 @click.command('get_results', short_help='Get list of results')
 def get_results():
     """Return the list of domains in EBI"""
-    ebisearch.get_results(verbose=True)
+    enasearch.get_results(verbose=True)
 
 
 @click.command('get_filter_fields', short_help='Get filter fields')
@@ -21,19 +21,19 @@ def get_results():
     help='Id of a result (accessible with get_results)')
 def get_filter_fields(result):
     """Get the filter fields of a result to build a query"""
-    ebisearch.get_filter_fields(verbose=True)
+    enasearch.get_filter_fields(verbose=True)
 
 
 @click.command('get_filter_types', short_help='Get filter types')
 def get_filter_types():
     """Get the types of filters usable to build a query"""
-    ebisearch.get_filter_types(verbose=True)
+    enasearch.get_filter_types(verbose=True)
 
 
 @click.command('get_display_options', short_help='Get display options')
 def get_display_options():
     """Get the display options to specify the display format"""
-    ebisearch.get_display_options(verbose=True)
+    enasearch.get_display_options(verbose=True)
 
 
 @click.command('get_download_options', short_help='Get download options')
@@ -41,7 +41,7 @@ def get_download_options():
     """Get the download options to specify that records are to be saved in a
     file
     """
-    ebisearch.get_download_options(verbose=True)
+    enasearch.get_download_options(verbose=True)
 
 
 main.add_command(get_results)
