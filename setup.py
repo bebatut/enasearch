c5c12518a2cc28564385bdf70958bea707dfc15c
Bérénice Batut <bebatut@gmail.com>
2017-08-17 11:21:26 +0200

    Update setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="enasearch",
-    version="0.0.5",
+    version="0.0.6",
     author="Berenice Batut",
     author_email="berenice.batut@gmail.com",
     description=("A Python library for interacting with ENA's API"),
